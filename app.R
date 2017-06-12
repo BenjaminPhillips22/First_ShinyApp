313ae76cb80b74a4a4b10152f5a8ce205192b306
Ben <benjamin.phillips@silverpond.com.au>
2017-06-12 14:00:47 +1000

    some updates

--- app.R
+++ app.R
@@ -11,10 +11,9 @@ library(ggplot2)
 library(dplyr)
 library(rsconnect)
 library(cowplot)
-library(shiny)
 
 #set wd
-#setwd("/home/ben/2016 Sem 1/Data Visualisation/Assignment3/Sal")
+setwd("C:/Users/ben/Documents/code/R/First_ShinyApp")
 
 # table1
 load("salary_field_gender")
@@ -44,14 +43,15 @@ ui <- shinyUI(fluidPage(
             tabsetPanel(type = "tabs",
                         tabPanel("Starting Salary", plotOutput("plot1")),
                         tabPanel("Separate by Gender", plotOutput("plot2")),
-                        tabPanel("About the Data", 
+                        tabPanel("About this App", 
                                  textOutput("text0"),
                                  textOutput("text1"),
                                  helpText(a("\n     Click here to view the download page, pick the Education xls file", href = "http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/4125.0Feb%202016?OpenDocument")),
-                                 helpText(a("\n     or click here to download the data set directly", href = "http://www.abs.gov.au/ausstats/SUBSCRIBER.NSF/log?openagent&41250ds0002_201602.xls&4125.0&Data%20Cubes&68EB79EDE3345E74CA257F61002084F0&0&Feb%202016&23.02.2016&Latest")),
+                                 helpText(a("\n     Click here to download the data set directly", href = "http://www.abs.gov.au/ausstats/SUBSCRIBER.NSF/log?openagent&41250ds0002_201602.xls&4125.0&Data%20Cubes&68EB79EDE3345E74CA257F61002084F0&0&Feb%202016&23.02.2016&Latest")),
+                                 helpText(a("\n     Click here to view the code that created this app", href = "https://github.com/BenjaminPhillips22/First_ShinyApp")),
                                  textOutput("text2")
-                        ),
-                        class = "rightAlign")
+                        )#,
+                        )#class = "rightAlign")
         )
     )#end of sidebarLayout
 ))
@@ -61,7 +61,7 @@ ui <- shinyUI(fluidPage(
 server <- shinyServer(function(input, output) {
     
     #set wd
-   # setwd("/home/ben/2016 Sem 1/Data Visualisation/Assignment3/Starting_Salary")
+   # setwd("C:/Users/ben/Documents/code/R/First_ShinyApp")
     
     # table1
     load("salary_field_gender")
@@ -69,22 +69,38 @@ server <- shinyServer(function(input, output) {
     # table2
     load("salary_field")
     
+    my_colours <- c("#9d6031","#546dd6","#a0b53c","#9f5ed1","#55b64f","#c74cae","#529d68","#dd4785","#4bc1b5","#cf4d2c","#4c9ad1","#e0883b","#9d95df","#c5a44d","#735a9e","#6d7930","#d585c4","#c94250","#a04b6d","#e18680")
+    my_fields <- c("Accounting","Agricultural Science","Architecture & Building","Art & Design ",
+                   "Biological Sciences","Computer Science","Dentistry","Earth Sciences",
+                   "Economics, Business","Education","Engineering","Humanities","Law","Mathematics",
+                  "Medicine","Paramedical Studies","Pharmacy (pre-reg)","Physical Sciences",
+                  "Psychology","Social Sciences")
+    
+    
+
     
     tt <- reactive({ as.character(input$GroupID) })
+    
+    
+    
     output$plot1 <- renderPlot({
-        #print(tt())
+        temp_colours <- my_colours[my_fields %in% tt()]
+        #print(temp_colours)
         g1 <- ggplot(table2 %>% filter( field %in% tt() ),
                      aes(x = year, y = avg.salary, colour = field))+
             geom_point()+
             geom_line() +
             labs(y = "Starting Salary (AU$ thousand)") +
-            ylim(c(30,90))
+            ylim(c(30,90))+
+            scale_colour_manual(values = temp_colours)
         g2 <- geom_line(data = table2 %>% filter(field == "Median"),
                         aes(x = year, y = avg.salary, colour = field), colour = "black")#+
         g3 <- geom_point(data = table2 %>% filter(field == "Median"),
                          aes(x = year, y = avg.salary, colour = field), colour = "black")#+
-        g4 <- g1+g2+g3
-        ggdraw(add_sub(g4, "\n *The black line in the median starting salary for all fields"))
+        g4 <- g1+g2+g3+
+          scale_x_continuous(breaks = seq(2001, 2015, 3))#+
+          #theme(axis.text.x = element_text(angle=80, hjust = 0.5, vjust = 0.5))
+        ggdraw(add_sub(g4, "\n *The black line is the median starting salary for all fields"))
         
     })
     
@@ -95,7 +111,10 @@ server <- shinyServer(function(input, output) {
             geom_point()+
             geom_line() +
             labs(y = "Starting Salary (AU$ thousand)") +
-            facet_grid(.~field)
+            facet_grid(.~factor(field, levels = c("Median", my_fields[my_fields %in% tt()] )))+
+            scale_x_continuous(breaks = seq(2001, 2015, 3))+
+            theme(axis.text.x = element_text(angle=80, hjust = 0.5, vjust = 0.5))+
+            scale_color_manual(values = c("#4bb092", "#b460bd"))
         g1
     })
     
